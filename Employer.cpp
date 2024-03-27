21ef342b564545950bbe3525b5990542592fe1ac
blopez <lopezz.bryann966@gmail.com>
2024-03-27 02:18:52 -0700

    Added a class calledEmployer with Employer.h and Employer.cpp to see how changes pop up on the git side of things hehehe.

---
+++ Employer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Bryan Lopez on 3/27/24.
+//
+
+#include "Employer.h"
---
+++ Employer.h
@@ -0,0 +1,15 @@
+//
+// Created by Bryan Lopez on 3/27/24.
+//
+
+#ifndef REVIEWPROJECT_EMPLOYER_H
+#define REVIEWPROJECT_EMPLOYER_H
+
+#include <iostream>
+class Employer
+{
+
+};
+
+
+#endif //REVIEWPROJECT_EMPLOYER_H
